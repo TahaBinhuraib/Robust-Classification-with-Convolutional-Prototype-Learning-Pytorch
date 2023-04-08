64fb77c989671f225acbebe10d1f7e876872fadb
TahaBinhuraib <taha.huraib@gmail.com>
2023-04-08 15:51:51 +0300

    resnet

--- Models.py
+++ Models.py
@@ -1,6 +1,4 @@
-
 from __future__ import print_function
-import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -8,12 +6,16 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from torch.autograd import Variable
+import torch
+import torch.nn as nn
+import torchvision
+import torch.nn.functional as F
 
 
 class Net(nn.Module):
-    def __init__(self, num_hidden_units=2, num_classes=10,s=2):
+    def __init__(self, num_hidden_units=2, num_classes=10, s=2):
         super(Net, self).__init__()
-        self.scale=s
+        self.scale = s
         self.conv1_1 = nn.Conv2d(1, 32, kernel_size=5, padding=2)
         self.prelu1_1 = nn.PReLU()
         self.conv1_2 = nn.Conv2d(32, 32, kernel_size=5, padding=2)
@@ -28,8 +30,7 @@ class Net(nn.Module):
         self.prelu3_2 = nn.PReLU()
         self.preluip1 = nn.PReLU()
         self.ip1 = nn.Linear(128 * 3 * 3, 2)
-        self.dce=dce_loss(num_classes,num_hidden_units)
-   
+        self.dce = dce_loss(num_classes, num_hidden_units)
 
     def forward(self, x):
         x = self.prelu1_1(self.conv1_1(x))
@@ -41,43 +42,86 @@ class Net(nn.Module):
         x = self.prelu3_1(self.conv3_1(x))
         x = self.prelu3_2(self.conv3_2(x))
         x = F.max_pool2d(x, 2)
-        x= x.view(-1, 128 * 3 * 3)
+        x = x.view(-1, 128 * 3 * 3)
         x1 = self.preluip1(self.ip1(x))
-        centers,x=self.dce(x1)
-        output = F.log_softmax(self.scale*x, dim=1)
-        return x1,centers,x,output
+        centers, x = self.dce(x1)
+        output = F.log_softmax(self.scale * x, dim=1)
+        return x1, centers, x, output
 
 
 class dce_loss(torch.nn.Module):
-    def __init__(self, n_classes,feat_dim,init_weight=True):
-   
+    def __init__(self, n_classes, feat_dim, init_weight=True):
+
         super(dce_loss, self).__init__()
-        self.n_classes=n_classes
-        self.feat_dim=feat_dim
-        self.centers=nn.Parameter(torch.randn(self.feat_dim,self.n_classes).cuda(),requires_grad=True)
+        self.n_classes = n_classes
+        self.feat_dim = feat_dim
+        self.centers = nn.Parameter(
+            torch.randn(self.feat_dim, self.n_classes).cuda(), requires_grad=True
+        )
         if init_weight:
             self.__init_weight()
 
     def __init_weight(self):
         nn.init.kaiming_normal_(self.centers)
 
-     
-
     def forward(self, x):
-   
-        features_square=torch.sum(torch.pow(x,2),1, keepdim=True)
-        centers_square=torch.sum(torch.pow(self.centers,2),0, keepdim=True)
-        features_into_centers=2*torch.matmul(x, (self.centers))
-        dist=features_square+centers_square-features_into_centers
+
+        features_square = torch.sum(torch.pow(x, 2), 1, keepdim=True)
+        centers_square = torch.sum(torch.pow(self.centers, 2), 0, keepdim=True)
+        features_into_centers = 2 * torch.matmul(x, (self.centers))
+        dist = features_square + centers_square - features_into_centers
 
         return self.centers, -dist
 
+
 def regularization(features, centers, labels):
-        distance=(features-torch.t(centers)[labels])
+    distance = features - torch.t(centers)[labels]
 
-        distance=torch.sum(torch.pow(distance,2),1, keepdim=True)
+    distance = torch.sum(torch.pow(distance, 2), 1, keepdim=True)
 
-        distance=(torch.sum(distance, 0, keepdim=True))/features.shape[0]
+    distance = (torch.sum(distance, 0, keepdim=True)) / features.shape[0]
 
-        return distance
+    return distance
 
+
+class ModifiedResNet(nn.Module):
+    def __init__(self, num_hidden_units, s, *args, **kwargs):
+        super(ModifiedResNet, self).__init__()
+        self.scale = s
+        self.resnet = torchvision.models.resnet18(pretrained=False)
+        num_ftrs = self.resnet.fc.in_features
+
+        self.reduce_to_hidden = nn.Linear(num_ftrs, num_hidden_units)
+        self.dce_loss = dce_loss(10, num_hidden_units)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        x1 = F.ReLU(self.reduce_to_hidden(x))
+        centers, x = self.dce_loss(x1)
+        output = F.log_softmax(self.scale * x, dim=1)
+        return x1, centers, x, output
+
+
+class dce_loss(torch.nn.Module):
+    def __init__(self, n_classes, feat_dim, init_weight=True):
+
+        super(dce_loss, self).__init__()
+        self.n_classes = n_classes
+        self.feat_dim = feat_dim
+        self.centers = nn.Parameter(
+            torch.randn(self.feat_dim, self.n_classes).cuda(), requires_grad=True
+        )
+        if init_weight:
+            self.__init_weight()
+
+    def __init_weight(self):
+        nn.init.kaiming_normal_(self.centers)
+
+    def forward(self, x):
+
+        features_square = torch.sum(torch.pow(x, 2), 1, keepdim=True)
+        centers_square = torch.sum(torch.pow(self.centers, 2), 0, keepdim=True)
+        features_into_centers = 2 * torch.matmul(x, (self.centers))
+        dist = features_square + centers_square - features_into_centers
+
+        return self.centers, -dist
